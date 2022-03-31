97063cef2113e6fc1d6ccf97341974560ba79248
Katrin3536 <Ekaterina_dr@tut.by>
2022-03-31 16:20:28 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [value, setValue] = useState<number>(0);
+
+    useEffect(() => {
+        let valueAsString = localStorage.getItem('counterValue');
+        if (valueAsString) {
+            let valueAsNumber = JSON.parse(valueAsString);
+            setValue(valueAsNumber);
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('counterValue', JSON.stringify(value));
+    }, [value]);
+
+    const incHandler = () => {
+        setValue(value + 1);
+
+    };
+    // const setToLocalStorage = () => {
+    //     localStorage.setItem('counterValue', JSON.stringify(value));
+    //     // localStorage.setItem('counterValue +1', JSON.stringify(value + 1));
+    // };
+
+    // const getFromLocalStorage = () => {
+    //     let valueAsString = localStorage.getItem('counterValue');
+    //     if (valueAsString) {
+    //         let valueAsNumber = JSON.parse(valueAsString);
+    //         setValue(valueAsNumber);
+    //     }
+    // };
+
+    // const clearLocalStorage = () => {
+    //     localStorage.clear();
+    //     setValue(0);
+    // };
+    //
+    // const removeItemclearLocalStorage = () => {
+    //     localStorage.removeItem('counterValue +1');
+    // };
+
+    return (
+        <div className="App">
+            <h1>{value}</h1>
+            <button onClick={incHandler}>inc</button>
+            {/*<button onClick={setToLocalStorage}>setToLocalStorage</button>*/}
+            {/*<button onClick={getFromLocalStorage}>getFromLocalStorage</button>*/}
+            {/*<button onClick={clearLocalStorage}>clearLocalStorage</button>*/}
+            {/*<button onClick={removeItemclearLocalStorage}>removeItemclearLocalStorage</button>*/}
+        </div>
+    );
 }
 
 export default App;
